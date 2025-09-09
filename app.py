0f5bf622f29491ae3456180eda13ed034b0aa26a
Tushar Arora <tushararora1235@gmail.com>
2025-09-09 10:51:18 +0530

    Deployment for Commit 1.10

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def predict():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
